51d40318ab73276d6eaa8f1a2e1329ab8cb30ea3
Colin <cstatro@gmail.com>
2019-08-20 11:43:54 -0400

    initial commit

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,2 +1,3 @@
 class PokemonsController < ApplicationController
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :wildmons
   resources :pokemons
   resources :users
+  get "/start-three/:id", to: "pokemons#start_three"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
