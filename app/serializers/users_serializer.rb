5daa269d7d7380ea5552091e6752f933853e0f56
IanGrubb <yangrubb@gmail.com>
2019-08-22 15:49:53 -0400

    food in serializer

--- app/serializers/users_serializer.rb
+++ app/serializers/users_serializer.rb
@@ -1,4 +1,4 @@
 class UsersSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name,:authority,:facility_tier,:facility_cleanliness,:pokemon_capacity,:pokemons
+  attributes :id, :name,:authority,:facility_tier,:facility_cleanliness,:pokemon_capacity,:pokemons, :food
 end
