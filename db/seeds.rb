83a33d8a5f4f1cbbce3e4006859320bdc8b9f64e
IanGrubb <hi@iangrubb.com>
2019-10-22 15:03:37 -0400

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Pokemon.destroy_all
-User.destroy_all 
-ash = User.create(name:"ash", facility_tier: 10)
-User.create(name:"brock")
+# Pokemon.destroy_all
+# User.destroy_all 
 
-Pokemon.generate_starters(ash.id)
+# ash = User.create(name:"ash", facility_tier: 10)
+# User.create(name:"brock")
+
+# Pokemon.generate_starters(ash.id)
 
 # Pokemon.destroy_all
 
@@ -31,40 +32,40 @@ Pokemon.generate_starters(ash.id)
 # Create Wildmon
 
 
-# def find_level_in_evo(poke_id, data)
+def find_level_in_evo(poke_id, data)
 
-#     check = data.evolves_to.find{|evo| evo.species.url.split("/").last.to_i == poke_id}
+    check = data.evolves_to.find{|evo| evo.species.url.split("/").last.to_i == poke_id}
 
-#     if check 
-#         if check.evolution_details[0].min_level
-#             check.evolution_details[0].min_level
-#         else
-#             40
-#         end
-#     else
-#         find_level_in_evo(poke_id, data.evolves_to[0])
-#     end
+    if check 
+        if check.evolution_details[0].min_level
+            check.evolution_details[0].min_level
+        else
+            40
+        end
+    else
+        find_level_in_evo(poke_id, data.evolves_to[0])
+    end
 
-# end
+end
 
 # Wildmon.destroy_all
 
 
-# (1..151).to_a.each do |id|
+(1..151).to_a.each do |id|
 
-#     species = PokeApi.get(pokemon_species: id)
+    species = PokeApi.get(pokemon_species: id)
 
     
 
-#     if species.evolves_from_species == nil
-#         min_lev = 1
-#     else
-#         evo_id = species.evolution_chain.url.split("/").last.to_i
-#         evolution_chain = PokeApi.get(evolution_chain: evo_id)
-#         min_lev = find_level_in_evo(id, evolution_chain.chain)
-#     end
+    if species.evolves_from_species == nil
+        min_lev = 1
+    else
+        evo_id = species.evolution_chain.url.split("/").last.to_i
+        evolution_chain = PokeApi.get(evolution_chain: evo_id)
+        min_lev = find_level_in_evo(id, evolution_chain.chain)
+    end
 
-#     Wildmon.create(species_id: id, habitat: species.habitat.name, minimum_level: min_lev, capture_rate: species.capture_rate)
+    Wildmon.create(species_id: id, habitat: species.habitat.name, minimum_level: min_lev, capture_rate: species.capture_rate)
           
     
-# end
+end
